7741e4c995e695cc48680cfde651c5ebf543f916
Eli K Miller <eli@elikmiller.com>
2018-09-28 18:27:30 -0500

    Added loader to refreshCampers. Minor visual improvements for campers page

--- src/Campers/CamperCardForm.jsx
+++ src/Campers/CamperCardForm.jsx
@@ -39,73 +39,80 @@ class CamperCardForm extends Component {
   render() {
     return (
       <div className="camper-card-form">
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="first-name">First Name</label>
-            <input
-              id="firstName"
-              type="input"
-              className="form-control form-control-sm"
-              onChange={this.handleChange}
-              value={this.state.formValues["firstName"]}
-            />
+        <div className="card mb-3">
+          <div className="card-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="first-name">First Name</label>
+                <input
+                  id="firstName"
+                  type="input"
+                  className="form-control form-control-sm"
+                  onChange={this.handleChange}
+                  value={this.state.formValues["firstName"]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="last-name">Last Name</label>
+                <input
+                  id="lastName"
+                  type="input"
+                  className="form-control form-control-sm"
+                  onChange={this.handleChange}
+                  value={this.state.formValues["lastName"]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="gender">Gender</label>
+                <select
+                  id="gender"
+                  className="custom-select"
+                  onChange={this.handleChange}
+                  value={this.state.formValues["gender"]}
+                >
+                  <option defaultValue>Choose a gender...</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                  <option value="unspecified">Unspecified</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="birth-date">Birth Date</label>
+                <input
+                  id="dateOfBirth"
+                  type="date"
+                  className="form-control form-control-sm"
+                  onChange={this.handleChange}
+                  value={this.state.formValues["dateOfBirth"]}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="notes">Notes</label>
+                <textarea
+                  id="notes"
+                  className="form-control form-control-sm"
+                  onChange={this.handleChange}
+                  value={this.state.formValues["notes"]}
+                />
+              </div>
+              <div className="form-group text-right">
+                <button
+                  className="btn btn-secondary mr-1"
+                  onClick={this.handleClose}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="btn btn-primary"
+                  type="submit"
+                  onClick={this.handleSubmit}
+                >
+                  Save
+                </button>
+              </div>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="last-name">Last Name</label>
-            <input
-              id="lastName"
-              type="input"
-              className="form-control form-control-sm"
-              onChange={this.handleChange}
-              value={this.state.formValues["lastName"]}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="gender">Gender</label>
-            <select
-              id="gender"
-              className="custom-select"
-              onChange={this.handleChange}
-              value={this.state.formValues["gender"]}
-            >
-              <option defaultValue>Choose a gender...</option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="unspecified">Unspecified</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="birth-date">Birth Date</label>
-            <input
-              id="dateOfBirth"
-              type="date"
-              className="form-control form-control-sm"
-              onChange={this.handleChange}
-              value={this.state.formValues["dateOfBirth"]}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="notes">Notes</label>
-            <textarea
-              id="notes"
-              className="form-control form-control-sm"
-              onChange={this.handleChange}
-              value={this.state.formValues["notes"]}
-            />
-          </div>
-          <div className="form-group">
-            <button
-              className="btn btn-primary"
-              type="submit"
-              onClick={this.handleSubmit}
-            >
-              Save
-            </button>
-            <button className="btn btn-secondary" onClick={this.handleClose}>
-              Cancel
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
