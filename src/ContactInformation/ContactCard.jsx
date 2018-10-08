96182033f08b676d8433d63c72cd18e6194cf1b7
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-10-07 22:20:03 -0500

    blank contact info no longer reads undefined

--- src/ContactInformation/ContactCard.jsx
+++ src/ContactInformation/ContactCard.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class ContactCard extends Component {
+  state = {
+    dataAvailable: false
+  };
+
+  componentWillReceiveProps(props) {
+    if (props.data.firstName) {
+      this.setState({
+        dataAvailable: true
+      });
+    }
+  }
   render() {
     let { data } = this.props;
     let extraData = Object.values(data).slice(2);
@@ -16,9 +27,11 @@ class ContactCard extends Component {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title text-muted">{this.props.title}</h5>
-            <h3 className="card-subtitle mb-2">
-              {`${data.firstName} ${data.lastName}`}
-            </h3>
+            <h4 className="card-subtitle mb-2">
+              {this.state.dataAvailable
+                ? `${data.firstName} ${data.lastName}`
+                : "Please add contact info"}
+            </h4>
             {dataDisplay}
             <button className="btn btn-primary" onClick={this.props.openForm}>
               Edit
