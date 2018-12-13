81ab6761f8042885ef406073f922adf14be23b6c
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2018-12-13 13:37:23 -0600

    fixed sorting tables to work properly in chrome

--- src/sort.js
+++ src/sort.js
@@ -5,9 +5,19 @@ const handleSort = (elem, data, sortStatus) => {
   // value to be sorted is nested or not.
   const sortedArray = data.sort((a, b) => {
     if (sortStatus[id]["ascending"]) {
-      return id === value ? a[id] > b[id] : a[id][value] > b[id][value];
-    } else return id === value ? a[id] < b[id] : a[id][value] < b[id][value];
+      if (id === value) return compare(a[id], b[id]);
+      else return compare(a[id][value], b[id][value]);
+    } else {
+      if (id === value) return compare(b[id], a[id]);
+      else return compare(b[id][value], a[id][value]);
+    }
   });
+
+  function compare(item1, item2) {
+    if (item1 < item2) return -1;
+    if (item1 > item2) return 1;
+    return 0;
+  }
 
   // update values and return data object
   Object.entries(sortStatus).forEach(([key, value]) => (value.engaged = false));
