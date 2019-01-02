7ebfcad952243d4ad9f70b4aea06682f38793740
Eli K Miller <eli@elikmiller.com>
2019-01-02 15:48:33 -0600

    Remove log statement

--- src/Admin/AdminCamps/AdminCamps.jsx
+++ src/Admin/AdminCamps/AdminCamps.jsx
@@ -5,7 +5,6 @@ import AdminCampDetails from "./AdminCampDetails";
 
 class AdminCamps extends Component {
   render() {
-    console.log(this.props.match);
     return (
       <Switch>
         <Route
