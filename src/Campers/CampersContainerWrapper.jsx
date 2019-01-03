8be09bcc6d57c81792b1c5da0ca361ee8f33888a
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2019-01-03 14:36:38 -0600

    campers and contact info now display properly on admin pages

--- src/Campers/CampersContainerWrapper.jsx
+++ src/Campers/CampersContainerWrapper.jsx
@@ -3,13 +3,13 @@ import { AuthContext } from "../App";
 import CampersContainer from "./CampersContainer";
 import "./Campers.css";
 
-const CampersContainerWrapper = () => {
+const CampersContainerWrapper = props => {
   return (
     <div className="wrapper campers-container-wrapper">
       <AuthContext.Consumer>
         {auth => (
           <CampersContainer
-            userId={auth.userId}
+            userId={props.userId || auth.userId}
             admin={auth.admin}
             logout={auth.logout}
           />
