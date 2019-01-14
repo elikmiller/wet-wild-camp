017452c19c3fbc0bed46b772ea1e9da02282c469
Wesley Maffly-Kipp <maffkipp@yahoo.com>
2019-01-13 23:28:24 -0600

    added delete functionality to admin camper detail page

--- client/src/Admin/AdminCampers/AdminCamperDetail.jsx
+++ client/src/Admin/AdminCampers/AdminCamperDetail.jsx
@@ -8,7 +8,9 @@ import { Link } from "react-router-dom";
 class AdminCamperDetail extends Component {
   state = {
     camper: {},
-    formOpen: false
+    formOpen: false,
+    confirmMessage: false,
+    errors: {}
   };
 
   componentDidMount() {
@@ -52,6 +54,38 @@ class AdminCamperDetail extends Component {
       });
   };
 
+  toggleConfirm = () => {
+    this.setState({
+      confirmMessage: !this.state.confirmMessage
+    });
+  };
+
+  deleteCamper = () => {
+    let id = this.state.camper._id;
+    appClient
+      .adminDeleteCamper(id)
+      .then(() => {
+        this.props.history.push("/admin/campers");
+      })
+      .catch(err => {
+        this.toggleConfirm();
+        if (err.response.status === 400) {
+          this.setState({
+            errors: {
+              delete:
+                "This camper has existing registrations and cannot be deleted."
+            }
+          });
+        } else if (err.response.status === 500) {
+          this.setState({
+            errors: { delete: "Could not communicate with server." }
+          });
+        } else {
+          this.setState({ errors: { delete: "Unknown Error." } });
+        }
+      });
+  };
+
   render() {
     let { camper } = this.state;
     let registrationArr;
@@ -65,16 +99,55 @@ class AdminCamperDetail extends Component {
     return (
       <div className="card">
         <div className="card-header">
-          <strong>Camper: </strong>
-          {camper.firstName} {camper.lastName}
-          <button
-            className="btn btn-primary btn-sm float-right"
-            onClick={this.toggleForm}
-          >
-            Edit
-          </button>
+          <div className="d-flex justify-content-between align-items-center">
+            <p className="mb-0">
+              <strong>Camper: </strong>
+              {camper.firstName} {camper.lastName}
+            </p>
+            <div>
+              <button
+                className="btn btn-primary btn-sm"
+                onClick={this.toggleForm}
+              >
+                <i className="fas fa-edit" /> Edit
+              </button>
+              <button
+                className="btn btn-danger btn-sm mb-0 ml-1"
+                onClick={this.toggleConfirm}
+              >
+                <i className="fas fa-trash" /> Delete
+              </button>
+            </div>
+          </div>
         </div>
         <div className="card-body">
+          {this.state.confirmMessage && (
+            <div
+              className="alert alert-info d-flex justify-content-between align-items-center"
+              role="alert"
+            >
+              Are you sure you want to delete this camper?
+              <div>
+                <button
+                  className="btn btn-secondary btn-sm"
+                  onClick={this.toggleConfirm}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="btn btn-danger btn-sm mb-0 ml-1"
+                  onClick={this.deleteCamper}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          )}
+          {this.state.errors.delete && (
+            <div className="alert alert-danger" role="alert">
+              {this.state.errors.delete}
+            </div>
+          )}
           <p className="card-text">
             <strong>Date of Birth: </strong>
             {this.formatDate(camper.dateOfBirth)}
