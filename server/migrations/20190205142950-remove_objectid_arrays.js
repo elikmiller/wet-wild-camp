9d4965545814bf2cef8103314447d2c1ef5620d8
Eli <eli.k.miller@gmail.com>
2019-02-08 08:35:20 -0600

    Replace update with updateMany

--- server/migrations/20190205142950-remove_objectid_arrays.js
+++ server/migrations/20190205142950-remove_objectid_arrays.js
@@ -2,11 +2,16 @@ module.exports = {
   async up(db) {
     await db
       .collection("camps")
-      .update({}, { $unset: { campers: 1, waitlist: 1 } });
-    await db.collection("campers").update({}, { $unset: { registrations: 1 } });
+      .updateMany({}, { $unset: { campers: 1, waitlist: 1 } });
+    await db
+      .collection("campers")
+      .updateMany({}, { $unset: { registrations: 1 } });
     await db
       .collection("users")
-      .update({}, { $unset: { campers: 1, registrations: 1, payments: 1 } });
+      .updateMany(
+        {},
+        { $unset: { campers: 1, registrations: 1, payments: 1 } }
+      );
     return Promise.resolve();
   },
 
